ac5be629cc496e8abf4fa71f6e9a0bdcebba0511
rpapraniku <Refik.Papraniku@endava.com>
2022-07-04 14:22:40 +0200

    update test

--- src/app/shared/services/github-api.service.spec.ts
+++ src/app/shared/services/github-api.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { GithubApiService } from './github-api.service';
 
 describe('GithubApiService', () => {
   let service: GithubApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(GithubApiService);
   });
 
